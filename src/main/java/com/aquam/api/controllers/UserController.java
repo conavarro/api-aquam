d284784fb1da80bccce9a1c5a898484db8630ed6
conavarro <44322821+conavarro@users.noreply.github.com>
2021-07-10 18:48:23 -0300

    intento de conectarse a la base

--- src/main/java/com/aquam/api/controllers/UserController.java
+++ src/main/java/com/aquam/api/controllers/UserController.java
@@ -1,5 +1,6 @@
-package com.aquam.api;
+package com.aquam.api.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
     @GetMapping("/")
     public ResponseEntity<?> getUsers(){
+        System.out.println(dbUrl);
         return ResponseEntity.ok("hola mundo!");
     }
 }
