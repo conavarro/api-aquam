76bf2322aca7fa3db77d38ec122e91419fd2101c
Conrado Navarro <conradonavarro6@gmail.com>
2021-07-18 15:12:37 -0300

    sacado imports innecesarios

--- src/main/java/com/aquam/api/ApiApplication.java
+++ src/main/java/com/aquam/api/ApiApplication.java
@@ -1,11 +1,7 @@
 package com.aquam.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class ApiApplication {
