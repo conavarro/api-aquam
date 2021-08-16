551732344e8f45e37e97a9244aa216a047e7d6f5
conavarro <44322821+conavarro@users.noreply.github.com>
2021-08-15 23:58:18 -0300

    agregado cors

--- src/main/java/com/aquam/api/controllers/ReportesController.java
+++ src/main/java/com/aquam/api/controllers/ReportesController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(
+        value = "*"
+)
 @RestController
 public class ReportesController {
 
