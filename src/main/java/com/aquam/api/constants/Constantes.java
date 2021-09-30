f10a49ead875e9b1f1e85beb96edab65ac7cd063
conavarro <conradonavarro6@gmail.com>
2021-09-30 19:20:38 -0300

    fix valor constante y reformat file

--- src/main/java/com/aquam/api/constants/Constantes.java
+++ src/main/java/com/aquam/api/constants/Constantes.java
@@ -3,5 +3,5 @@ package com.aquam.api.constants;
 public class Constantes {
     public static final String BASURA = "BASURA";
     public static final String DESAGUE_TAPADO = "DESAGÜE TAPADO";
-    public static final String TACHO_DESBORDADO = "TACHO_DESBORDADO";
+    public static final String TACHO_DESBORDADO = "TACHO DESBORDADO";
 }
